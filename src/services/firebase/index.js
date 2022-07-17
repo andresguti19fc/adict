0d0bf5a15a1d017953b81282a42e51f9975dfcad
andresguti19fc <chavogutierrez19@gmail.com>
2022-07-17 10:40:52 -0300

    s

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,17 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCzCfSKFKDIvft7cOxLVgZZoFOSjzWwIRw",
-  authDomain: "adicta-react.firebaseapp.com",
-  projectId: "adicta-react",
-  storageBucket: "gs://adicta-react.appspot.com",
-  messagingSenderId: "655642992268",
-  appId: "1:655642992268:web:d2ee2411da885dbdcde5a9"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
- 
+
 export const db = getFirestore(app);
