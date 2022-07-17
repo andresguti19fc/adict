fff79ef6f2c26486db96f173e656a529c039d814
andresguti19fc <chavogutierrez19@gmail.com>
2022-07-17 20:28:12 -0300

    as

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,23 @@
-import { faCashRegister, faGifts, faHeartbeat } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+/* import { faCashRegister, faGifts, faHeartbeat } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; */
 
 const Card = () => {
   const cards = [
     {
       title: "Elige como pagar.",
-      icon: faCashRegister,
+      icon: /* faCashRegister */ "cash-register",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, optio. At, dolorum officia incidunt",
     },
     {
       title: "Envios Gratis desde $ 5000.",
-      icon: faGifts,
+      icon: /* faGifts */ "gifts",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ",
     },
     {
       title: "Seguridad desde principio a fin.",
-      icon: faHeartbeat,
+      icon: /* faHeartbeat */ "heartbeat",
       description:
         " sapiente dolor pariatur quo aut veritatis cum sed magnam voluptas placeat, dolore fuga ipsum voluptatem mollitia.",
     },
@@ -27,7 +27,7 @@ const Card = () => {
     <>
       {cards.map((card, index) => (
         <div key={index} className="col-lg-3 p-3">
-          <FontAwesomeIcon icon={card.icon} size="3x" />
+          {/* <FontAwesomeIcon icon={card.icon} size="3x" /> */}
           <h3 className="text-center">{card.title}</h3>
           <p className="text-center">{card.description}</p>
         </div>
