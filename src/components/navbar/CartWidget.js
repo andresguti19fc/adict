1865cdcf388dac6792106c162f46f250ea43a8d1
andresguti19fc <chavogutierrez19@gmail.com>
2022-07-18 09:39:04 -0300

    as

--- src/components/navbar/CartWidget.js
+++ src/components/navbar/CartWidget.js
@@ -1,6 +1,6 @@
 
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+/* import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; */
 import './CartWidget.css';
 import { useContext } from 'react';
 import  CartContext  from '../../context/CartContext.js';
@@ -12,7 +12,7 @@ const CartWidget = () => {
 
     return (
         <Link to="cart" className={totalCantidad !== 0 ? 'btn btn-outline-light visible' : 'invisible' } >
-            <FontAwesomeIcon icon={faCartShopping} />
+            {/* <FontAwesomeIcon icon={faCartShopping} /> */}
         <span className="spanCounter"> { totalCantidad } </span>
         </Link>
        );
